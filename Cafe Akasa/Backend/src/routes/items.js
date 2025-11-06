d0be68c61b3aa1ddaf78fd23ddcd8c968f933d04
AKHIL KUMAR GOPE <akhil.gope@mca.christuniversity.in>
2025-11-06 19:44:34 +0000

    fix

--- Cafe Akasa/Backend/src/routes/items.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCategories, getItems } = require('../controllers/itemController');
-
-router.get('/categories', getCategories);
-router.get('/', getItems);
-
-module.exports = router;
-
